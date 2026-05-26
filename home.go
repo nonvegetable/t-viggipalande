d0412860059715e3bad4258f77a19c76a11efded
nonvegetable <vpalande81203@outlook.com>
2026-05-27 00:11:53 +0530

    initialised project

---
+++ home.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+func getHomeView(m Model, width, height int) string {
+	homeStyle := m.renderer.NewStyle().
+		Width(width).
+		Height(height)
+
+	heroText := `██╗   ██╗██╗ ██████╗  ██████╗ ██╗
+██║   ██║██║██╔════╝ ██╔════╝ ██║
+██║   ██║██║██║  ███╗██║  ███╗██║
+╚██╗ ██╔╝██║██║   ██║██║   ██║██║
+ ╚████╔╝ ██║╚██████╔╝╚██████╔╝██║
+  ╚═══╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝
+                                 
+Inspired by ` + createHyperlink("https://terminal.shop", "terminal.shop") + `and` + createHyperlink("https://sa1.dev", "sa1.dev") + `
+Go to ` + createHyperlink("https://viggipalande.live", "viggipalande.live") + ` for web version`
+
+	homeContent := m.renderer.NewStyle().
+		Width(width - 6).
+		Height(lipgloss.Height(heroText)).
+		Align(lipgloss.Center).
+		Render(heroText)
+
+	return homeStyle.Render(lipgloss.Place(
+		width, height,
+		lipgloss.Center, lipgloss.Center,
+		homeContent,
+	))
+}
